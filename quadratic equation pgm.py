7189635ae70792c5f281587d305298252c8eca20
Reshmi <91744955+Reshmisivaraj@users.noreply.github.com>
2021-11-21 12:52:18 +0530

    Update quadratic equation pgm.py

--- quadratic equation pgm.py
+++ quadratic equation pgm.py
@@ -4,5 +4,5 @@ b =float(input("Enter the coefficient of b :"))
 c =float(input("Enter the constant term :"))
 d =b**2-4ac
 x1 =((-b)+ (math.sqrt(d)))/(2*a)
-x2 =(-b)- (math.sqrt(d)))/(2*a)
-print("Rootsofthe quadratic equation are :",x1,x2)
+x2 =((-b)- (math.sqrt(d)))/(2*a)
+print("Roots ofthe quadratic equation are :",x1,x2)
