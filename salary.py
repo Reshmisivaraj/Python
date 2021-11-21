770e83ba636732a5d3e5fdc2c8d7ae011f5e38dd
Reshmi <91744955+Reshmisivaraj@users.noreply.github.com>
2021-11-21 12:51:06 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -8,6 +8,6 @@ medical_insurance =eval(input("enter the value of medical_insurance"))
 deduction=15/100(dearness_allowances+3/100(telephonoc_allowances))+10/100(medical_insurance)
 gross_pay=basic_salary+dearness_allowances+telephonoc_allowances+vehicle_allowances
 net_salry=gross_pay_deduction
-print=("gross pay is:",gross_pay)
+print("gross pay is:",gross_pay)
 print("total deduction is:",deduction)
 print("net salry is:",net_salry)
